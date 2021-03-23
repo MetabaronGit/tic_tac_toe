16f9ccc3bb0e2026446f097a2a42c08d7eb45c53
rad <cepelka.jr@email.cz>
2021-03-23 16:39:59 +0100

    23-02

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-SEPARATOR = "=" * 40
+SCREEN_WIDTH = 41
+separator = "=" * SCREEN_WIDTH
 
 def draw_greeting() -> None:
     """
@@ -8,8 +9,8 @@ def draw_greeting() -> None:
     """
     print()
     print("Welcome to Tic Tac Toe")
-    print(SEPARATOR)
-    print("GAME RULES:".center(len(SEPARATOR)))
+    print(separator)
+    print("GAME RULES:".center(SCREEN_WIDTH))
     print("""Each player can place one mark (or stone)
 per turn on the 3x3 grid. The WINNER is
 who succeeds in placing three of their
@@ -17,12 +18,46 @@ marks in a:
 * horizontal,
 * vertical or
 * diagonal row""")
-    print(SEPARATOR)
-    print("Let's start the game".center(len(SEPARATOR)))
+    print(separator)
+    print("Let's start the game".center(SCREEN_WIDTH))
+    print(separator)
+
+
+def draw_board(game_board: list) -> None:
+    """
+    Vykreslí aktuální stav hrací plochy.
+
+    :return: None
+    """
+    print("+---+---+---+".center(SCREEN_WIDTH))
+    print("|{:^3}|{:^3}|{:^3}|".format(game_board[7], game_board[8], game_board[9]).center(SCREEN_WIDTH))
+    print("+---+---+---+".center(SCREEN_WIDTH))
+    print("|{:^3}|{:^3}|{:^3}|".format(game_board[4], game_board[5], game_board[6]).center(SCREEN_WIDTH))
+    print("+---+---+---+".center(SCREEN_WIDTH))
+    print("|{:^3}|{:^3}|{:^3}|".format(game_board[1], game_board[2], game_board[3]).center(SCREEN_WIDTH))
+    print("+---+---+---+".center(SCREEN_WIDTH))
+
+
+def draw_message(message: str) -> None:
+    """
+    Vypíše zprávu pro hráče
+
+    :param message: zpráva
+    :return: None
+    """
+    print(separator)
+    print(message.center(SCREEN_WIDTH))
+    print(separator)
 
 
 def main():
     draw_greeting()
+    # v listu je schválně o 1 pole víc, aby souhlasily indexy polí s tlačítky čísel
+    game_board = ["", "", "x", "", "o", "x", "", "", "", ""]
+    draw_board(game_board)
+    game_messages = ["Player {} | Please enter your move number: ", "Congratulations, the player {} WON!"]
+    # is_player_o() kolečkář začíná
+    draw_message("pokuston")
 
 
 if __name__ == "__main__":
